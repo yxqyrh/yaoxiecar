c5f56337695d9aedefaa5ed865733ce7da614a18
jingyaxie <jingyaxiegm@gmail.com>
2016-01-05 17:29:10 +0800

    新增邀请码，车辆管理界面

--- washcar/UserCenterViewController.h
+++ washcar/UserCenterViewController.h
@@ -18,4 +18,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *userPhone;
 @property (weak, nonatomic) IBOutlet UILabel *surplusMoney;
 
+@property (weak, nonatomic) IBOutlet UIButton *btn1;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
+@property (weak, nonatomic) IBOutlet UIButton *btn3;
+@property (weak, nonatomic) IBOutlet UIButton *btn4;
+@property (weak, nonatomic) IBOutlet UIButton *btn5;
+@property (weak, nonatomic) IBOutlet UIButton *btn6;
+
+@property (weak, nonatomic) IBOutlet UIButton *btn7;
+@property (weak, nonatomic) IBOutlet UIButton *btn8;
+@property (weak, nonatomic) IBOutlet UIView *btnbody;
+
 @end
