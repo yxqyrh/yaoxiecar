7d171f3db76c5dcc83bc726432878baa857bb88f
jingyaxie <jingyaxiegm@gmail.com>
2016-01-23 02:05:24 +0800

    投诉列表开发完成

--- washcar/ComplaintListViewController.m
+++ washcar/ComplaintListViewController.m
@@ -8,6 +8,8 @@
 
 #import "ComplaintListViewController.h"
 #import "MayiHttpRequestManager.h"
+#import "StringUtil.h"
+#import "DateUtil.h"
 @interface ComplaintListViewController (){
     NSMutableArray *array;
 }
@@ -37,7 +39,8 @@
     [rightBarButtonItem setTintColor:[UIColor whiteColor]];
 
     self.navigationItem.rightBarButtonItem = rightBarButtonItem;
-    
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     [self  loadData:YES];
 }
 
@@ -55,50 +58,75 @@
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     
 
-    return 200;
+    UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
+    return cell.backgroundView.frame.size.height;
     
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    static NSString *identifier = @"ComplaintListCell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault   reuseIdentifier:identifier];
-    }
-    
     NSDictionary *dic = array[indexPath.row];
-    UITextView *title =  (UITextView*)[cell viewWithTag:1];
-    UILabel *time =  (UILabel*)[cell viewWithTag:2];
-    UITextView *content =  (UITextView*)[cell viewWithTag:3];
-    UIView *view1 = [cell viewWithTag:4];
-    UIView *view2 = [cell viewWithTag:5];
-   
-//    content.scrollEnabled = NO;
-    title.text = [dic objectForKey:@"mes"];
-    time.text = [dic objectForKey:@"time"];
-    
-    NSString *str = [dic objectForKey:@"content"];
-    if (str== nil||str.length == 0) {
-        str = @"暂无回复";
-    }
-    content.text = str;
-    
-    
+
     UIFont *font_mes = [UIFont systemFontOfSize:15];
     // 該行要顯示的內容
     NSString *mes = [dic objectForKey:@"mes"];
     // 計算出顯示完內容需要的最小尺寸
-    CGSize size_mes = [mes sizeWithFont:font_mes constrainedToSize:CGSizeMake(title.frame.size.width, 1000.0f) lineBreakMode:UILineBreakModeWordWrap];
+    CGSize size_mes = [mes sizeWithFont:font_mes constrainedToSize:CGSizeMake(SCREEN_WIDTH-100, 1000.0f) lineBreakMode:UILineBreakModeWordWrap];
     UIFont *font_content = [UIFont systemFontOfSize:14];
     // 該行要顯示的內容
     NSString *content_str = [dic objectForKey:@"content"];
+    if (content_str==nil||content_str.length==0) {
+        content_str = @"暂无回复";
+    }
     // 計算出顯示完內容需要的最小尺寸
-    CGSize size_content = [content_str sizeWithFont:font_content constrainedToSize:CGSizeMake(content.frame.size.width, 1000.0f) lineBreakMode:UILineBreakModeWordWrap];
-    view1.frame = CGRectMake(0, 0, SCREEN_WIDTH, size_mes.height);
-//    view2.frame = CGRectMake(0, view2.frame.origin.y, SCREEN_WIDTH, size_content.height);
+    CGSize size_content = [content_str sizeWithFont:font_content constrainedToSize:CGSizeMake(SCREEN_WIDTH, 1000.0f) lineBreakMode:UILineBreakModeWordWrap];
+    
+    UIView *body1 = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, size_mes.height+8)];
+    
+    UILabel *content1 = [[UILabel alloc]initWithFrame:CGRectMake(8, 4, SCREEN_WIDTH-110, size_mes.height)];
+    content1.lineBreakMode = UILineBreakModeWordWrap;
+    content1.numberOfLines = 0;//上面两行设置多行显示
+    content1.font = font_mes;
+    content1.text = [dic objectForKey:@"mes"];
+    UILabel *time = [[UILabel alloc]initWithFrame:CGRectMake(SCREEN_WIDTH-90, 8, 95, 15)];
+    NSString *sjc =[dic objectForKey:@"time"];
+    NSString *time_text = [DateUtil nsdateToString:[DateUtil zoneChange:sjc] :@"yyyy-MM-dd"];
+    time.text = time_text;
+    time.font =[UIFont systemFontOfSize:13];
+    time.textColor = [UIColor redColor];
+    [body1 addSubview:content1];
+    [body1 addSubview:time];
     
     
+    
+
+    
+    
+    UIView *body2 = [[UIView alloc]initWithFrame:CGRectMake(0, body1.frame.size.height, SCREEN_WIDTH, size_content.height+16+15)];
+    body2.backgroundColor =  [UIColor colorWithRed:242/255.0 green:242/255.0 blue:242/255.0 alpha:1.0];
+    
+    UIImageView *icon = [[UIImageView alloc]initWithFrame:CGRectMake(8, 8, 15, 15)];
+    icon.image = [UIImage imageNamed:@"complaint_table_item_icon.png"];
+    UILabel *lable1 = [[UILabel alloc]initWithFrame:CGRectMake(26, 8, 100, 15)];
+    lable1.font = [UIFont systemFontOfSize:14];
+    lable1.text = @"掌柜回复";
+    lable1.textColor =[UIColor grayColor];
+    
+    UILabel *content_text = [[UILabel alloc]initWithFrame:CGRectMake(26, 25, SCREEN_WIDTH-16, size_content.height)];
+    content_text.textColor =[UIColor grayColor];
+    content_text.font =font_content;
+    content_text.text =content_str;
+    [body2 addSubview:icon];
+    [body2 addSubview:lable1];
+    [body2 addSubview:content_text];
+    UIView *body_all = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, body1.frame.size.height+body2.frame.size.height)];
+    [body_all addSubview:body1];
+    [body_all addSubview:body2];
+    static NSString *identifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault   reuseIdentifier:identifier];
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.backgroundView = body_all;
     return cell;
     
 }
